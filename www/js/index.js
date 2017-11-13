b854d7ca23ee905f7d1b3cfec9f0d8b363f0c550
VavrisX <vavl1_16@uni.worc.ac.uk>
2017-11-13 10:13:47 +0000

    xx

---
+++ www/js/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    
+    
+    
+    
+
+
+
+
+
+function random() {
+    
+var Rnum = Math.floor((Math.random() * 10) + 1);
+    
+if(Rnum >= 1 && Rnum < 5){$("#myText").text("TRUE");
+  navigator.notification.beep(1);                       
+  navigator.vibrate(2000);                     
+   } 
+
+else { $("#myText").text("FALSE");
+  navigator.notification.beep(2);      
+   navigator.vibrate([200, 300, 500]);      
+         }
+    
+}
+}
