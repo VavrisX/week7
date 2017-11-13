1c1b3cc8658cf298481988bc988cfbc2ab6ab4cb
VavrisX <vavl1_16@uni.worc.ac.uk>
2017-11-13 11:41:04 +0000

    xx

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,19 @@
+ console.log("js loaded");
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
- function onDeviceReady() {
-       alert(navigator.notification);   
+ 
+function onDeviceReady() {
+     console.log("onDeviceReady");
+    console.log(navigator.notification);
+    alert(navigator.notification);   
  }
      
      
 $(document).on("pagecreate","#pageone",function(){
+    console.log("pagecreate");
     $('#stuff').on("click", function(){
+        console.log("click");
         random();
     });            
 });      
@@ -16,18 +23,19 @@ $(document).on("pagecreate","#pageone",function(){
 
 
 function random() {
+    console.log("random");
     
     var Rnum = Math.floor((Math.random() * 10) + 1);
     
     if(Rnum >= 1 && Rnum < 5){
         $("#myText").text("TRUE");
-        navigator.notification.beep(1);  
+       // navigator.notification.beep(1);  
         navigator.vibrate(2000);                     
     } 
 
     else { 
         $("#myText").text("FALSE");
-        navigator.notification.beep(2);      
+       // navigator.notification.beep(2);      
         navigator.vibrate([200, 300, 500]);      
     }
     
